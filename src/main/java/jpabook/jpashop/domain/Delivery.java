c08321eadd2d9e0e2c8c8f2195a079bd1bb578ce
minyugyeong <alsdbrud123@naver.com>
2025-02-01 15:11:06 +0900

    섹션3. 도메인 분석 설계 - 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,8 +13,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
