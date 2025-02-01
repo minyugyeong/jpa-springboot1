048842dcbda7338255795eee820fe55df94c4aa4
minyugyeong <alsdbrud123@naver.com>
2025-02-01 16:32:40 +0900

    섹션5. 회원 도메인 개발 - 회원 기능 테스트

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.ex.Hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
